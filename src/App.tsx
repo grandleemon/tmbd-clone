db552a218011478a03beb998f3d28c7a651de3c4
Max <zeetreex@gmail.com>
2022-01-07 17:25:36 +0200

    installed tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex w-full justify-between">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
     </div>
   );
 }
